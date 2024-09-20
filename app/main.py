0f4ada6d2ab49d74c22458457d1afa2b4d856cf7
Alex <toer1xe@gmail.com>
2024-09-20 11:13:35 +0200

    expanded project structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+# from routers import users, receipts
 
-app = FastAPI()
+app = FastAPI(
+    title="Receipt API",
+    description="This is an API for creating and viewing receipts with user registration and authentication.",
+    version="1.0.0",
+    contact={
+        "name": "Oleksandr Chaban",
+        "email": "toer1xe@gmail.com",
+    },
+)
+
+# app.include_router(users.router, prefix="/users", tags=["users"])
+# app.include_router(receipts.router, prefix="/receipts", tags=["receipts"])
 
 @app.get("/")
 def read_root():
